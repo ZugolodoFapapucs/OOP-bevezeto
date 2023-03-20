046de3212b622689e8253af6d5eabe8771fa5244
bsze2molbal <bsze2molbal@SZT1-6-HP>
2023-03-20 17:47:10 +0100

    myCat példányosítása

--- bevezeto_projekt/bevezeto_projekt/Program.cs
+++ bevezeto_projekt/bevezeto_projekt/Program.cs
@@ -25,4 +25,27 @@ public class Cat
 /// </summary>
 public class Dog
 {
+    /// <summary>
+    /// O1.04 Polimorfizmus (többalakúság)
+    ///  - Egyazon objektum többfélének látszódhat.
+    ///  - Több különböző fajta objektum ugyanolyannak látszódhat.
+    ///  Itt a második esetről van szó. Mindegyik osztály (Cat, Dog...) ugyan úgy,
+    ///  a beszél művelettel "beszél", de mindegyik különböző dolgokat mond.
+    /// </summary>
+    public void Say()
+    {
+        Console.WriteLine("The dog says: bow wow");
+    }
 }
+
+// O1.05 OOP programozás
+// Az OOP programozás során a programokat objektumokból építjük fel.
+//  - Minden objektumnak megvan a jól meghatározott feladata.
+//  - Az objektumok képesek egymással kommunikálni.
+//  - OOP programozás során a programozást közelítjük az emberi gondolkodáshoz.
+
+// O1.06 Példányosítás
+// Cat osztály példányosítása (create instance of class).
+//  - A myCat objektum a Cat osztály egy példánya.
+//  - A lokális változókat írásakor a caneCaseing jelölést használunk.
+Cat myCat = new Cat();
